71e6583d5ed11da0cecd97caccb0664489a19724
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-01-25 19:52:12 +0900

    add:Api/TodoController

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -1,11 +1,34 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TodoController extends Controller
 {
-    //
+    public function __construct(
+      private Todo $todo
+    ) {}
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required', 'string', 'max:255']
+        ]);
+        $this->todo->fill($validated)->save();
+
+        return ['message' => 'ok'];
+    }
+
 }
+
