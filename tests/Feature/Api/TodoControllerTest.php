5256262c2aa080cf3d7bb1cae636c095e9488ffd
kousuke-funabshi <128987004+kousuke-funabshi@users.noreply.github.com>
2024-01-28 15:42:32 +0900

    test:Todoの新規作成が成功するテスト

--- tests/Feature/Api/TodoControllerTest.php
+++ tests/Feature/Api/TodoControllerTest.php
@@ -25,15 +25,21 @@ class TodoControllerTest extends TestCase
     public function Todoの新規作成()
     {
 
-        $todo = Todo::factory()->state([
-            'title' => 'テスト',
-            'content' => 'テスト',
-        ])->create();
+        $params = [
+            'title' => 'テスト:タイトル',
+            'content' => 'テスト:内容'
+        ];
 
-        $this->assertDatabaseHas('todos', [
-            'title' => 'テスト',
-            'content' => 'テスト',
-        ]);
+        $res = $this->postJson(route('api.todo.create'), $params);
+        $res->assertOk();
+        $todos = Todo::all();
+
+        $this->assertCount(1, $todos);
+
+        $todo = $todos->first();
+
+        $this->assertEquals($params['title'], $todo->title);
+        $this->assertEquals($params['content'], $todo->content);
     }
 
     /**
